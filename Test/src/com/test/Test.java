afda3998014d325e337d75d95c0a325a9a5ccd74
ab33767 <ab33767@XIN009ODAA71654.ctl.intranet>
2015-07-14 15:02:21 +0530

    adding sum code

--- Test/src/com/test/Test.java
+++ Test/src/com/test/Test.java
@@ -14,9 +14,14 @@ public class Test {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int sum=0;
            for(int i=0;i<5;i++){
         	   System.out.println(i);
+        	   sum+=i;
            }
+           
+           System.out.println("sum is "+ sum);
 	}
 
 }
