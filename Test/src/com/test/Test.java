3d000e1958d832bfcab3f796d8d86c26c651db70
ab33767 <ab33767@XIN009ODAA71654.ctl.intranet>
2015-07-14 14:50:38 +0530

    adding project to helloworld repository

---
+++ Test/src/com/test/Test.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.test;
+
+/**
+ * @author ab33767
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           for(int i=0;i<5;i++){
+        	   System.out.println(i);
+           }
+	}
+
+}
